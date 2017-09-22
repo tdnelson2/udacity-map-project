5a0b5eb80b02cc90f1dfb115716d6cb72e9f169c
Tim Nelson <tdnelson@gmail.com>
2017-09-22 11:03:27 -0700

    rearrange order of locations

--- js/models/model.js
+++ js/models/model.js
@@ -2,36 +2,6 @@ var Model = {
 	// Partner data
 	Locations : [
 		{
-			title: 'Rwanda Partners',
-			city: 'Kigali',
-			country: 'Rwanda',
-			description: 'Founded by a local pastor who survived the genocide, our Rwandan Ministry partners have always been focused on bringing hope and healing through restored relationships. Since 1996 they have been serving orphans in the country\'s capital city of Kigali. Now through partnership with Lahash, the ministry is expanding into the rural outskirts of the city where a new generation of vulnerable children are struggling against poverty, disease, and lack of education. Through the local church they are reaching out to change these young lives with the love and good news of Jesus.',
-			coordinates: {
-				lat: -1.953768,
-				lng: 30.096305
-			},
-			searchTerms: ['Rwanda']
-		}, {
-			title: 'Kampala House',
-			city: 'Kampala',
-			country: 'Uganda',
-			description: 'Uganda has become home to many South Sudanese refugees who fled during Sudan\'s seemingly endless cycle of civil war and political instability. Kampala House exist to provide safe refuge for children whose lives have been marked by indescribable losses. For many of them, the ministry has offered the only stable living situation they have ever known.',
-			coordinates: {
-				lat: 0.311491,
-				lng: 32.574313
-			},
-			searchTerms: ['Kampala']
-		}, {
-			title: 'Grace & Healing Ministry',
-			city: 'Dodoma',
-			country: 'Tanzania',
-			description: 'In Dodoma, Tanzania, a group of women at a local church were moved by the plight of those in their community suffering with HIV/AIDS, so they met, prayed, and then founded a ministry. Lahash is honored to partner with the passionate and creative staff of Grace & Healing Ministry as they make a positive difference in so many lives. Children once at high risk of extreme neglect due to their poor health and poverty now thrive as they attend school, live in stable homes, and fill the halls of a local church with laughter.',
-			coordinates: {
-				lat: -6.158543,
-				lng: 35.744421
-			},
-			searchTerms: ['Grace & Healing Ministry', 'Dodoma']
-		}, {
 			title: 'Nipe Tumaini',
 			city: 'Nairobi',
 			country: 'Kenya',
@@ -51,6 +21,37 @@ var Model = {
 				lng: 33.424168
 			},
 			searchTerms: ['Path of Hope', 'Shinyanga']
+		}, {
+			title: 'Grace & Healing Ministry',
+			city: 'Dodoma',
+			country: 'Tanzania',
+			description: 'In Dodoma, Tanzania, a group of women at a local church were moved by the plight of those in their community suffering with HIV/AIDS, so they met, prayed, and then founded a ministry. Lahash is honored to partner with the passionate and creative staff of Grace & Healing Ministry as they make a positive difference in so many lives. Children once at high risk of extreme neglect due to their poor health and poverty now thrive as they attend school, live in stable homes, and fill the halls of a local church with laughter.',
+			coordinates: {
+				lat: -6.158543,
+				lng: 35.744421
+			},
+			searchTerms: ['Grace & Healing Ministry', 'Dodoma']
+		}, 
+		{
+			title: 'Rwanda Partners',
+			city: 'Kigali',
+			country: 'Rwanda',
+			description: 'Founded by a local pastor who survived the genocide, our Rwandan Ministry partners have always been focused on bringing hope and healing through restored relationships. Since 1996 they have been serving orphans in the country\'s capital city of Kigali. Now through partnership with Lahash, the ministry is expanding into the rural outskirts of the city where a new generation of vulnerable children are struggling against poverty, disease, and lack of education. Through the local church they are reaching out to change these young lives with the love and good news of Jesus.',
+			coordinates: {
+				lat: -1.953768,
+				lng: 30.096305
+			},
+			searchTerms: ['Rwanda']
+		}, {
+			title: 'Kampala House',
+			city: 'Kampala',
+			country: 'Uganda',
+			description: 'Uganda has become home to many South Sudanese refugees who fled during Sudan\'s seemingly endless cycle of civil war and political instability. Kampala House exist to provide safe refuge for children whose lives have been marked by indescribable losses. For many of them, the ministry has offered the only stable living situation they have ever known.',
+			coordinates: {
+				lat: 0.311491,
+				lng: 32.574313
+			},
+			searchTerms: ['Kampala']
 		}
 	],
 
